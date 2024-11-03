31114b3ba788476d5b77263cd5735db28d619183
Keerti Krishna Sreenivas S <2300031039@kluniversity.in>
2024-11-03 09:45:01 +0530

    03.11.2024.java

---
+++ November/03.11.2024.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isLengthEven(Node head) {
+        // code here
+        int count =0;
+        while (head != null)
+        {
+            head= head.next;
+            count++;
+            
+        }
+        if(count % 2 == 0)
+        {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+}
